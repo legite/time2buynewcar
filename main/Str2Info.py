41e9b4c010230203944e298a97d8761cb36880d7
lea <2273914403@qq.com>
2017-10-14 19:08:25 +0800

    Initial commit

---
+++ main/Str2Info.py
@@ -0,0 +1,12 @@
+
+#定义字符串转换类
+class Str2Info(object):
+
+    def __init__(self, str):
+        ss = str.split('|')
+        self.id = ss[0]
+        self.date = ss[1]
+        self.branch = ss[2]
+        self.distance = int(ss[3])
+        Fix = {'T': True, 'F':False}
+        self.fix = Fix[ss[4]]
